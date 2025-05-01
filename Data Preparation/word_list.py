6d76d8f64d9cebe9e5295c693dd58fddb5bc9116
missa7481 <69221842+missa7481@users.noreply.github.com>
2025-05-01 18:29:49 -0400

    Update word_list.py

--- Data Preparation/word_list.py
+++ Data Preparation/word_list.py
@@ -110,8 +110,8 @@ damage_words_english = [
 ]
 
 damage_words_japanese = [
-    "停電",       # blackout
-    "壊れた",     # broke, broken
+    "停電",       # blackout，poweroutage
+    "壊れた",壊れる　壊れている　壊れちゃった# broke, broken
     "燃える",     # burn
     "燃えた",     # burned
     "燃えている",  # burning
@@ -121,11 +121,11 @@ damage_words_japanese = [
     "崩壊",       # collapse, collapsed, collapses
     "ひび",       # crack, cracked, cracking, cracks
     "墜落",       # crash, crashed, crashes
-    "無力",       # cripple, cripples
-    "崩れる",     # crumble
-    "押しつぶす",  # crush, crushed, crushes
-    "損傷",       # damage, damaged, damaging
-    "死んだ",     # dead, died, die, dies
+    "無力", ''       # cripple, cripples, helpless
+    "崩れる",崩壊     # crumble
+    "押しつぶす",つぶれた  # crush, crushed, crushes
+    "損傷", ダメージ      # damage, damaged, damaging
+    "死んだ"   # dead, died, die, dies
     "死亡",       # death, deaths
     "変形する",    # deform, deformed, deforms
     "破壊",       # destruct, destructed, destructing, destructs
@@ -133,26 +133,27 @@ damage_words_japanese = [
     "壊滅させる",  # devastate, devastated, devastates, devastating
     "死ぬ",       # die, died, dies
     "避難する",    # displace, displaced
-    "混乱する",    # disrupt, disrupted, disrupting, disrupts
-    "死者",       # fatalities, fatality
+    "混乱する", 混乱した   # disrupt, disrupted, disrupting, disrupts
+    "死者",  犠牲者     # fatalities, fatality
     "裂け目",     # fissure, fissures
     "火事",       # fire
     "洪水",       # flood, flooded, flooding
-    "傷つく",     # hurt, hurting, hurts
-    "けが",       # injuries, injury
-    "負傷した",    # injured
-    "殺す",       # kill, killed, killing
+    "傷つく",  傷ついた   # hurt, hurting, hurts
+    "けが",    ケガ　怪我  外傷 # injuries, injury
+    "負傷した",    怪我した# injured
+    "殺す",     殺された  # kill, killed, killing
     "漏れ",       # leak, leaked, leaking, leaks
-    "巨大な",     # massive
+    "巨大な", 大きな    # massive
     "がけ崩れ",   # rockslide
+    土砂崩れ　土石流　Landslide
     "瓦礫",       # rubble
     "破裂",       # rupture, ruptures
     "安全",       # safe
     "散らす",     # scatter, scattered, scatters
-    "厳しい",     # severe
+    "厳しい",  大変  # severe
     "粉々にする",  # shatter, shattered, shatters
     "打ち砕く",   # smash, smashed, smashes, smashing
     "苦しむ",     # suffer, suffered, suffering, suffers
-    "外傷",       # trauma
+    "トラウマ",       # trauma
     "ゆがむ",     # warp, warps
 ]
